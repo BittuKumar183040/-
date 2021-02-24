25b9eabe75b2d62b63954a9baa4fb2a37f360bbe
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2021-02-24 22:30:22 +0530

    Add files via upload

--- mouseEffect.js
+++ mouseEffect.js
@@ -2,15 +2,17 @@
 
 const riple=document.querySelector('#riple');
 riple.style.zIndex=4;
+const mouseRun=()=>{
+    window.addEventListener('mousemove', (event)=>{
+        riple.style.top=event.clientY+"px";
+        riple.style.left=event.clientX+"px";
+        riple.style.opacity=".5";
+    })
+    window.addEventListener('mouseout', ()=>{
+        riple.style.opacity="0";
+    })
+}
 
-window.addEventListener('mousemove', (event)=>{
-    riple.style.top=event.clientY+"px";
-    riple.style.left=event.clientX+"px";
-    riple.style.opacity=".5";
-})
-window.addEventListener('mouseout', ()=>{
-    riple.style.opacity="0";
-})
 // window.addEventListener('click',()=>{
 //     riple.style.opacity=1;
 // })
