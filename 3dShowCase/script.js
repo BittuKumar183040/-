3cab153e28c5dc682a14304dad9d21debeb51b92
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2023-10-06 18:53:22 +0530

    updated experience

--- 3dShowCase/script.js
+++ 3dShowCase/script.js
@@ -1,5 +1,6 @@
 const greet=document.getElementById("greetings")
-const date=parseInt(new Date().getHours());
+// const date=parseInt(new Date().getHours());
+let date=1;
 if(date<12 && date>0){
     greetings.innerText="Good Morning";
 }
