867a017a21bf696bd168bc6286df718b63c05d05
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2023-10-06 12:41:52 +0530

    Fix hard coded value

--- 3dShowCase/script.js
+++ 3dShowCase/script.js
@@ -1,6 +1,5 @@
 const greet=document.getElementById("greetings")
-// const date=parseInt(new Date().getHours());
-let date=1;
+const date=parseInt(new Date().getHours());
 if(date<12 && date>0){
     greetings.innerText="Good Morning";
 }
